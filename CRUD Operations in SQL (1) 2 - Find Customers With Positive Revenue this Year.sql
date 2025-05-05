0d13358823e485493523d67624f250215617ba95
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-05 23:23:13 +0530

    Update CRUD Operations in SQL (1) 2 - Find Customers With Positive Revenue this Year.sql

--- CRUD Operations in SQL (1) 2 - Find Customers With Positive Revenue this Year.sql
+++ CRUD Operations in SQL (1) 2 - Find Customers With Positive Revenue this Year.sql
@@ -1,7 +1,9 @@
 Find Customers With Positive Revenue this Year
 
+'''
 Write an SQL query to report the unique customers with postive revenue in the year 2021.
 Note: Use distinct operator to get unique values.
+'''
 
 QUERY:
 
