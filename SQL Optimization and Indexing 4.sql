aea11b3b28e7f868ad72a68af3e16926b5d02c68
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-07 23:25:30 +0530

    Create SQL Optimization and Indexing 4.sql

---
+++ SQL Optimization and Indexing 4.sql
@@ -0,0 +1,16 @@
+Benefit of B-Tree in MYSQL
+
+What is the primary benefit of using a B-tree index in MySQL?
+
+Choose the correct answer from below:
+  
+1. It is faster for text search operations.
+
+2. It requires less disk space than other index types
+
+3. It efficiently supports equality and range queries.
+
+4. It can index complex data types like arrays and JSON.
+
+Ans:
+It efficiently supports equality and range queries.
