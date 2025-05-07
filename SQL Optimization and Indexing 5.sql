430466eac7f8166722cff34cae6e8b0afc5347d8
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-07 23:26:17 +0530

    Create SQL Optimization and Indexing 5.sql

---
+++ SQL Optimization and Indexing 5.sql
@@ -0,0 +1,12 @@
+SQL statement used to execution plan
+
+Which SQL statement is used to view the execution plan of a query in MySQL?
+
+Choose the correct answer from below:
+  
+1. DESCRIBE
+
+2. EXPLAIN
+
+Ans:
+EXPLAIN
