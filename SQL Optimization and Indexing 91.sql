66117981b9ea1ee1220d4b1b9f099478bb876032
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-09 23:16:27 +0530

    Create SQL Optimization and Indexing 91.sql

---
+++ SQL Optimization and Indexing 91.sql
@@ -0,0 +1,16 @@
+Create an Index in MYSQL
+
+Which command is used to create an index in MySQL?
+
+Choose the correct answer from below:
+  
+1. CREATE TABLE
+
+2. CREATE INDEX
+
+3. ALTER TABLE
+
+4. CREATE VIEW
+
+Ans:
+CREATE INDEX
