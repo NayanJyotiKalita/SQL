a47485c6c22fcf1ac48aaf49777d7c50a9a3a329
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-10 23:25:19 +0530

    Create Advanced Indexing and Transactions 5.sql

---
+++ Advanced Indexing and Transactions 5.sql
@@ -0,0 +1,16 @@
+Transaction scenario with concurrent updates
+
+In a transaction scenario with concurrent updates, which isolation level would likely result in a serialization failure in MySQL?
+
+Choose the correct answer from below:
+  
+1. Read Uncommitted
+
+2. Read Committed
+
+3. Repeatable Read
+
+4. Serializable
+
+Ans:
+Serializable
