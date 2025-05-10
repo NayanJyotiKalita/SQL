b50aea7cecaade360cd8624693c36d75fe8c9781
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-10 23:21:54 +0530

    Create Advanced Indexing and Transactions 2.sql

---
+++ Advanced Indexing and Transactions 2.sql
@@ -0,0 +1,16 @@
+`SAVEPOINT` command in MySQL
+
+What does the SAVEPOINT command in MySQL allow you to do?
+
+Choose the correct answer from below:
+  
+1. Start a new transaction.
+
+2. Commit all changes made in the current transaction.
+
+3. Set a point within a transaction to which you can later roll back.
+
+4. Cancel the current transaction without saving changes.
+
+Ans:
+Set a point within a transaction to which you can later roll back.
