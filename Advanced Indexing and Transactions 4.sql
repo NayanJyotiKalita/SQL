f25610936ab9aa448a7d639128a42a0f24a2023f
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-10 23:24:17 +0530

    Create Advanced Indexing and Transactions 4.sql

---
+++ Advanced Indexing and Transactions 4.sql
@@ -0,0 +1,16 @@
+Default Isolation Level
+
+Which isolation level is the default in MySQL and balances performance with data consistency?
+
+Choose the correct answer from below:
+  
+1. Read Uncommitted
+
+2. Read Committed
+
+3. Repeatable Read
+
+4. Serializable
+
+Ans:
+Repeatable Read
