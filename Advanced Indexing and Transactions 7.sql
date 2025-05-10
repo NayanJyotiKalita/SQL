e4f8a33a7197b1c7cf35b802259089245015714d
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-10 23:27:44 +0530

    Create Advanced Indexing and Transactions 7.sql

---
+++ Advanced Indexing and Transactions 7.sql
@@ -0,0 +1,16 @@
+Query uses an index in MYSQL
+
+How can you check if a query uses an index in MySQL?
+
+Choose the correct answer from below:
+  
+1. Use the EXPLAIN command.
+
+2. Use the DESCRIBE command.
+
+3. Use the ANALYZE command.
+
+4. Use the SELECT command.
+
+Ans:
+Use the EXPLAIN command.
