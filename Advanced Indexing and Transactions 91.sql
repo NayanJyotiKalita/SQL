f2e630789bed43947aed81e5805a9fbdf2ced6d3
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-12 23:46:56 +0530

    Create Advanced Indexing and Transactions 91.sql

---
+++ Advanced Indexing and Transactions 91.sql
@@ -0,0 +1,16 @@
+Rollback to a SAVEPOINT
+  
+What happens when you rollback to a SAVEPOINT in a MySQL transaction?
+
+Choose the correct answer from below:
+  
+1. All changes made after the SAVEPOINT are committed.
+
+2. The transaction is canceled entirely.
+
+3. All changes made after the SAVEPOINT are undone.
+
+4. The SAVEPOINT is deleted from the transaction.
+
+Ans:
+All changes made after the SAVEPOINT are undone.
