99715d736710b32dbbc168b1ef9ce795ba8283a1
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-12 23:48:12 +0530

    Create Advanced Indexing and Transactions 92.sql

---
+++ Advanced Indexing and Transactions 92.sql
@@ -0,0 +1,16 @@
+Serializable isolation level
+
+Which of the following statements is true about the Serializable isolation level in MySQL?
+
+Choose the correct answer from below:
+
+1. It allows dirty reads.
+
+2. It allows non-repeatable reads.
+
+3. It ensures complete isolation by simulating serial execution of transactions.
+
+4. It is the fastest isolation level.
+
+Ans:
+It ensures complete isolation by simulating serial execution of transactions.
