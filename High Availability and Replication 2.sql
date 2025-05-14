4bfe97124a7f1e1b02971623885f54af316142f1
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-14 23:29:23 +0530

    Create High Availability and Replication 2.sql

---
+++ High Availability and Replication 2.sql
@@ -0,0 +1,16 @@
+Key component of a High Availability
+
+Which of the following is a key component of a High Availability architecture in MySQL?
+
+Choose the correct answer from below:
+  
+1. Indexing
+
+2. Redundancy
+
+3. Compression
+
+4. Partitioning
+
+Ans:
+Redundancy
