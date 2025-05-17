9c288e67915254c9427b7af875f630a291fe0dfe
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-17 23:43:16 +0530

    Create Managing Roles and Users in MySQL.sql

---
+++ SQL Administration/Managing Roles and Users in MySQL.sql
@@ -0,0 +1,18 @@
+Assignment 1: Managing Roles and Users in MySQL
+  
+Objective:
+Create a role with specific privileges and assign a user to this role in the scaler database.
+
+Instructions:
+Task 1: Create a Role with Specific Privileges
+- Create a role named read_order that has read-only access to the orders table in the scaler database.
+- Ensure that the role has the necessary privileges to perform SELECT operations on the orders table.
+
+Task 2: Create a User and Assign the Role
+- Create a new user named Ram with password ram@123! .
+- Assign the read_order role to the user Ram so that they can read data from the orders table in the scaler database.
+- Verify that the user Ram has the correct privileges by testing their access to the orders table.
+
+COMMAND:
+
+
