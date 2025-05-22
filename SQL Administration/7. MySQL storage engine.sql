d53fd4421b383582be8c49eab2e0d61fea1250a1
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-22 23:45:40 +0530

    Update and rename 7.sql to 7. MySQL storage engine.sql

---
+++ SQL Administration/7. MySQL storage engine.sql
@@ -0,0 +1,16 @@
+MySQL storage engine
+
+Which MySQL storage engine supports foreign keys and transactions?
+
+Choose the correct answer from below:
+
+1. MyISAM
+
+2. InnoDB
+
+3. MEMORY
+
+4. CSV
+
+Ans:
+InnoDB
