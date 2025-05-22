ac9d423dd72e09c04d9d246a669be1a7cc45d427
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-22 23:43:57 +0530

    Update and rename 6. sql to 106. Function of GRANT Command.sql

---
+++ SQL Administration/106. Function of GRANT Command.sql
@@ -0,0 +1,16 @@
+Function of GRANT Command
+
+What is the function of the GRANT command in MySQL?
+
+Choose the correct answer from below:
+
+1. To remove user privileges
+
+2. To delete a user account
+
+3. To assign privileges to a user
+
+4. To optimize the database
+
+Ans:
+To assign privileges to a user
