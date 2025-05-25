af0c62ab556c1f27f6db700a607e504d50467024
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-25 22:37:15 +0530

    Update and rename 90.sql to 110. EXPLAIN STATEMENT.sql

---
+++ SQL Administration/110. EXPLAIN STATEMENT.sql
@@ -0,0 +1,16 @@
+`EXPLAIN STATEMENT`
+
+What does the EXPLAIN statement do in MySQL?
+  
+Choose the correct answer from below:
+
+1. It backs up the database.
+
+2. It shows how MySQL executes a query.
+
+3. It creates a new user.
+
+4. It deletes a table.
+
+Ans:
+It shows how MySQL executes a query.
