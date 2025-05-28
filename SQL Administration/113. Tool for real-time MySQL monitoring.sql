c5b8635a9dc0cf4f5522201910c156e1ffb04ad5
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-28 23:02:59 +0530

    Create 113. Tool for real-time MySQL monitoring.sql

---
+++ SQL Administration/113. Tool for real-time MySQL monitoring.sql
@@ -0,0 +1,16 @@
+Tool for real-time MySQL monitoring
+
+Which tool is commonly used for real-time MySQL monitoring?
+
+Choose the correct answer from below:
+  
+1. mysqldump
+
+2. MySQL Workbench
+
+3. MySQL Router
+
+4. mysqlsecureinstallation
+
+Ans:
+MySQL Workbench
