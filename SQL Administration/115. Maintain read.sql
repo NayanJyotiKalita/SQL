a3c01a85640537f9ad6a38fd3c857fca6a5cbcdd
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-28 23:04:43 +0530

    Create 115. Maintain read.sql

---
+++ SQL Administration/115. Maintain read.sql
@@ -0,0 +1,16 @@
+Maintain read
+
+Which MySQL feature allows the database to maintain read consistency?
+
+Choose the correct answer from below:
+
+1. Indexing
+
+2. Replication
+
+3. Isolation levels
+
+4. Logging
+
+Ans:
+Isolation levels
