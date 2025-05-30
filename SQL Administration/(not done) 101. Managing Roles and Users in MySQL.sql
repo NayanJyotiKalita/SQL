ab35c03f6f9735d0577b36f4c89cd23db897a3cc
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-30 23:59:56 +0530

    Update (not done) 101. Managing Roles and Users in MySQL.sql

--- SQL Administration/(not done) 101. Managing Roles and Users in MySQL.sql
+++ SQL Administration/(not done) 101. Managing Roles and Users in MySQL.sql
@@ -12,7 +12,7 @@ Task 2: Create a User and Assign the Role
 - Create a new user named Ram with password ram@123! .
 - Assign the read_order role to the user Ram so that they can read data from the orders table in the scaler database.
 - Verify that the user Ram has the correct privileges by testing their access to the orders table.
-
+ 
 COMMAND:
 
 
