179f52be4b527ba7f8ccd1f723d8d0f135d21110
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-06-25 23:01:53 +0530

    Update and rename 101. Find Customer Referee.sql to 101. Find Customer Referee.md

--- 102. CRUD Operations in SQL (1)/101. Find Customer Referee.sql
+++
@@ -1,9 +0,0 @@
-Find Customer Referee
-
-Write an SQL query to fetch the names of the customer that are not referred by the customer with id = 102.
-
-QUERY:
-
-SELECT name 
-from customer 
-where referee_id != 102;
